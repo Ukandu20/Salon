af48750aa4908bcf814df2f33776ac4738c66ea9
O_Ukandu <100975029+Ukandu20@users.noreply.github.com>
2024-05-07 17:26:26 -0400

    modified:   backend/package-lock.json 	modified:   backend/package.json 	modified:   frontend/package.json 	modified:   frontend/src/axiosConfig.js

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
 axios.defaults.baseURL =
-    process.env.NODE_ENV === 'development' ? 'http://localhost:5000/' : '/';
+    process.env.NODE_ENV === 'development' ? 'http://localhost:5000/' : 'https://salon-1.onrender.com/';
