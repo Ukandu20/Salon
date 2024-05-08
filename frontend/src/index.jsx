13e526642c1c4616e9728c467edcdd1b33070515
O_Ukandu <100975029+Ukandu20@users.noreply.github.com>
2024-05-08 07:27:05 -0400

    modified:   frontend/package-lock.json 	modified:   frontend/package.json 	new file:   frontend/src/.env 	modified:   frontend/src/components/Booking/Booking.module.css 	modified:   frontend/src/index.css 	modified:   frontend/src/index.jsx 	modified:   frontend/src/pages/Appointments/Appointments.module.css 	modified:   frontend/tailwind.config.js

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-        <ChakraProvider> 
+        <ChakraProvider resetCSS={false} disableGlobalStyle={true}> 
           <App />
         </ChakraProvider>
     </BrowserRouter>
